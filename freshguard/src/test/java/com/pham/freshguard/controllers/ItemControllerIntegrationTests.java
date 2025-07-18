3da375b9c640e8895a2e083d04c7dc9d09744241
khuengocminhpham <pham178@purdue.edu>
2025-07-18 12:24:30 +0700

    complete tests for item

--- freshguard/src/test/java/com/pham/freshguard/controllers/ItemControllerIntegrationTests.java
+++ freshguard/src/test/java/com/pham/freshguard/controllers/ItemControllerIntegrationTests.java
@@ -7,8 +7,6 @@ import com.pham.freshguard.TestDataUtil;
 import com.pham.freshguard.domain.entities.ItemEntity;
 import com.pham.freshguard.services.ItemService;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,11 +21,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.time.LocalDate;
-
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @Transactional
 @Rollback
 @AutoConfigureMockMvc
@@ -53,26 +48,26 @@ public class ItemControllerIntegrationTests {
     @Test
     public void testThatCreateItemSuccessfullyReturnsHttp201Created() throws Exception {
         ItemEntity itemEntity = TestDataUtil.createTestItemEntityA();
-        String authorJson = objectMapper.writeValueAsString(itemEntity);
+        String itemJson = objectMapper.writeValueAsString(itemEntity);
 
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/items")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(authorJson)
+                        .content(itemJson)
         ).andExpect(
                 MockMvcResultMatchers.status().isCreated()
         );
     }
 
     @Test
-    public void testThatCreateItemSuccessfullyReturnsSavedAuthor() throws Exception {
+    public void testThatCreateItemSuccessfullyReturnsSavedItem() throws Exception {
         ItemEntity itemEntity = TestDataUtil.createTestItemEntityA();
-        String authorJson = objectMapper.writeValueAsString(itemEntity);
+        String itemJson = objectMapper.writeValueAsString(itemEntity);
 
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/items")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(authorJson)
+                        .content(itemJson)
         ).andExpect(
                 MockMvcResultMatchers.jsonPath("$.id").isNumber()
         ).andExpect(
@@ -132,5 +127,158 @@ public class ItemControllerIntegrationTests {
                 MockMvcRequestBuilders.get("/items/" + savedItem.getId())
                         .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testThatGetItemReturnsItemWhenItemExist() throws Exception {
+        ItemEntity itemEntity = TestDataUtil.createTestItemEntityA();
+        ItemEntity savedItem = itemService.save(itemEntity);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/items/" + savedItem.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").value(savedItem.getId())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.name").value("Salt")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.category").value("Spices")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.expirationDate").value("2026-01-16")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.purchaseDate").value("2025-07-16")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.quantity").value(1)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.location").value("Pantry")
+        );
+    }
+
+    @Test
+    public void testThatGetItemReturnsHttpStatus404WhenNoItemExists() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/items/9999")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testThatFullUpdateItemReturnsHttpStatus404WhenNoItemExists() throws Exception {
+        ItemEntity item = TestDataUtil.createTestItemEntityA();
+        String itemJson = objectMapper.writeValueAsString(item);
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/items/9999")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(itemJson)
+        ).andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testThatFullUpdateItemReturnsHttpStatus200WhenItemExists() throws Exception {
+        ItemEntity item = TestDataUtil.createTestItemEntityA();
+        ItemEntity savedItem = itemService.save(item);
+
+        String itemJson = objectMapper.writeValueAsString(item);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/items/" + savedItem.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(itemJson)
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testThatFullUpdateUpdatesExistingItem() throws Exception {
+        ItemEntity itemEntityA = TestDataUtil.createTestItemEntityA();
+        ItemEntity savedItem = itemService.save(itemEntityA);
+
+        ItemEntity itemEntityB = TestDataUtil.createTestItemEntityB();
+        itemEntityB.setId(savedItem.getId());
+
+        String itemUpdatedJson = objectMapper.writeValueAsString(itemEntityB);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/items/" + savedItem.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(itemUpdatedJson)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").value(savedItem.getId())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.name").value(itemEntityB.getName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.category").value(itemEntityB.getCategory())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.expirationDate").value("2025-07-22")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.purchaseDate").value("2025-07-16")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.quantity").value(itemEntityB.getQuantity())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.location").value(itemEntityB.getLocation())
+        );
+    }
+
+    @Test
+    public void testThatPartialUpdateExistingItemReturnsHttpStatus20Ok() throws Exception {
+        ItemEntity item = TestDataUtil.createTestItemEntityA();
+        ItemEntity savedItem = itemService.save(item);
+
+        item.setName("UPDATED");
+        String itemJson = objectMapper.writeValueAsString(item);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.patch("/items/" + savedItem.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(itemJson)
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testThatPartialUpdateExistingItemReturnsUpdatedItem() throws Exception {
+        ItemEntity item = TestDataUtil.createTestItemEntityA();
+        ItemEntity savedItem = itemService.save(item);
+
+        ItemEntity newItem = new ItemEntity();
+        newItem.setName("UPDATED");
+        String itemJson = objectMapper.writeValueAsString(newItem);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.patch("/items/" + savedItem.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(itemJson)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").value(savedItem.getId())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.name").value("UPDATED")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.category").value(savedItem.getCategory())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.expirationDate").value("2026-01-16")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.purchaseDate").value("2025-07-16")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.quantity").value(savedItem.getQuantity())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.location").value(savedItem.getLocation())
+        );
+    }
+
+    @Test
+    public void testThatDeleteItemReturnsHttpStatus204ForNonExistingItem() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/items/9999")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    public void testThatDeleteItemReturnsHttpStatus204ForExistingItem() throws Exception {
+        ItemEntity item = TestDataUtil.createTestItemEntityA();
+        ItemEntity savedItem = itemService.save(item);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/items/" + savedItem.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 }
