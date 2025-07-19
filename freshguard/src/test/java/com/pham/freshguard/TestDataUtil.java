b25ff39596bc66321cbfb98c92f7ab96f474d411
khuengocminhpham <pham178@purdue.edu>
2025-07-19 17:41:13 +0700

    initialize recipes and ingredients in objects

--- freshguard/src/test/java/com/pham/freshguard/TestDataUtil.java
+++ freshguard/src/test/java/com/pham/freshguard/TestDataUtil.java
@@ -6,6 +6,7 @@ import org.assertj.core.util.Sets;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,10 +18,11 @@ public class TestDataUtil {
         return ItemEntity.builder()
                 .name("Salt")
                 .category("Spices")
-                .expirationDate(LocalDate.of(2025, 7, 16).plusMonths(6)) // example expiration
+                .expirationDate(LocalDate.of(2025, 7, 16).plusMonths(6))
                 .purchaseDate(LocalDate.of(2025, 7, 16))
                 .quantity(1)
                 .location("Pantry")
+                .recipes(new HashSet<>())
                 .build();
     }
 
@@ -29,7 +31,7 @@ public class TestDataUtil {
         return ItemEntity.builder()
                 .name("Pork")
                 .category("Meat")
-                .expirationDate(LocalDate.of(2025, 7, 16).plusDays(6)) // example expiration
+                .expirationDate(LocalDate.of(2025, 7, 16).plusDays(6))
                 .purchaseDate(LocalDate.of(2025, 7, 16))
                 .quantity(2)
                 .location("Pantry")
@@ -41,7 +43,7 @@ public class TestDataUtil {
         return ItemEntity.builder()
                 .name("Milk")
                 .category("Diary")
-                .expirationDate(LocalDate.now().plusDays(6)) // example expiration
+                .expirationDate(LocalDate.now().plusDays(6))
                 .purchaseDate(LocalDate.now())
                 .quantity(1)
                 .location("Fridge")
@@ -56,6 +58,7 @@ public class TestDataUtil {
                 .servings(1)
                 .prepTimeMinutes(20)
                 .createdAt(LocalDateTime.now())
+                .ingredients(new HashSet<>())
                 .build();
     }
     public static RecipeEntity createTestRecipeEntityB() {
