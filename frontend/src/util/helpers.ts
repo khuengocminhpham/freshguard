921e3dbd85e6bb1bb8333f9a0dbca9f798fa5658
khuengocminhpham <pham178@purdue.edu>
2025-07-29 16:15:44 +0700

    change date format

--- frontend/src/util/helpers.ts
+++ frontend/src/util/helpers.ts
@@ -4,11 +4,11 @@ export const dateToArray = (date: Date): number[] => {
 
 export const arrayToDate = (dateArray: number[]): string => {
   if (dateArray.length >= 3) {
-    return new Date(
-      dateArray[0],
-      dateArray[1] - 1,
-      dateArray[2]
-    ).toLocaleDateString();
+    const date = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // add 1 since month is 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`; // format: YYYY-MM-DD
   }
-  return new Date().toLocaleDateString();
+  return new Date().toISOString().split("T")[0];
 };
