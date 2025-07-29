35fa413b86d2b21e9e74c09d8963f2115a544d03
khuengocminhpham <pham178@purdue.edu>
2025-07-29 10:20:41 +0700

    finish home page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Item } from "./pages/Item.tsx";
 import { Recipe } from "./pages/Recipe.tsx";
 import { ItemsProvider } from "./context/ItemsContext.tsx";
+import { RecipesProvider } from "./context/RecipesContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ItemsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/item" element={<Item />} />
-          <Route path="/recipe" element={<Recipe />} />
-        </Routes>
-      </BrowserRouter>
-    </ItemsProvider>
+    <RecipesProvider>
+      <ItemsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/item" element={<Item />} />
+            <Route path="/recipe" element={<Recipe />} />
+          </Routes>
+        </BrowserRouter>
+      </ItemsProvider>
+    </RecipesProvider>
   </StrictMode>
 );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,49 @@
+import { useEffect } from "react";
 import { ItemCard } from "../components/ItemCard";
 import { RecipeCard } from "../components/RecipeCard";
+import { useItems } from "../context/ItemsContext";
+import { useRecipes } from "../context/RecipesContext";
+import { Link } from "react-router";
 
-export const Home = () => {
+function Home() {
+  const { items, getItems } = useItems();
+  const { recipes, getRecipes } = useRecipes();
+  useEffect(() => {
+    getRecipes();
+    getItems();
+  }, []);
+
   return (
-    <div>
-      <h1>Fresh Guard!</h1>
-      <div className="flex flex-row justify-around w-full">
-        <div>
-          <ItemCard />
+    <div className="p-20">
+      <h1 className="mb-20 text-4xl font-extrabold tracking-tight leading-none text-primary-900 md:text-5xl lg:text-6xl">
+        Fresh Guard!
+      </h1>
+      <div className="flex flex-row justify-between gap-64">
+        <div className="flex flex-col gap-12">
+          <Link
+            to="/item"
+            className="hover:text-green-900 text-2xl font-bold tracking-tight  leading-none text-green-700 md:text-3xl lg:text-4xl"
+          >
+            Ingredients
+          </Link>
+          {items.map((item) => (
+            <ItemCard key={item.id} data={item} />
+          ))}
         </div>
-        <div>
-          <RecipeCard />
+        <div className="flex flex-col gap-12">
+          <Link
+            to="/recipe"
+            className="hover:text-green-900 text-2xl font-bold tracking-tight  leading-none text-green-700 md:text-3xl lg:text-4xl"
+          >
+            Recipes
+          </Link>
+          {recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} data={recipe} />
+          ))}
         </div>
       </div>
     </div>
   );
-};
+}
+
+export default Home;
