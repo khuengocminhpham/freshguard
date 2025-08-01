4d33b95678ec1f346d21bcfe895c5bca9b9dd03d
khuephamngocminh <pham178@purdue.edu>
2025-08-01 15:28:24 +0700

    change links

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,28 +13,25 @@ function Home() {
     getItems();
   }, []);
 
-
   return (
     <div className="p-20">
       <h1 className="mb-20 text-4xl font-extrabold tracking-tight leading-none text-primary-900 md:text-5xl lg:text-6xl">
-        Fresh Guard!
+        Welcome!
       </h1>
       <div className="flex flex-row justify-between gap-32">
         <div className="flex flex-col gap-12">
           <Link
-            to="/item"
+            to="/items"
             className="hover:text-green-900 text-2xl font-bold tracking-tight  leading-none text-green-700 md:text-3xl lg:text-4xl"
           >
             Ingredients
           </Link>
-          {items && items.map((item) => (
-            <ItemCard key={item.id} data={item} />
-          ))}
+          {items && items.map((item) => <ItemCard key={item.id} data={item} />)}
           {items.length == 0 && <div>No item found</div>}
         </div>
         <div className="flex flex-col gap-12">
           <Link
-            to="/recipe"
+            to="/recipes"
             className="hover:text-green-900 text-2xl font-bold tracking-tight  leading-none text-green-700 md:text-3xl lg:text-4xl"
           >
             Recipes
